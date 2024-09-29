b4c64f78529e5278adfafd8a5a368d0747a16c9f
muktadir1280 <muktadir1280@gmail.com>
2024-09-29 17:47:17 +0600

    modified responsive

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   theme: {
+//     screens: {
+//       sm: "576px",
+//       // => @media (min-width: 576px) { ... }
+
+//       md: "960px",
+//       // => @media (min-width: 960px) { ... }
+
+//       lg: "1440px",
+//       // => @media (min-width: 1440px) { ... }
+//     },
+//   },
+// };
+
+//[@media(min-width:711px)]
+
+//tailwind.config = { theme: { extend: { colors: { clifford: '#da373d', } } } }
+
+//screens: { '3xl': '1600px', // Add a custom breakpoint named '3xl' // You can customize existing breakpoints as well 'lg': {'max': '1023px'}, // Customize 'lg' breakpoint to max-width: 1023px },
+
+{
+  /* <script>
+    tailwind.config = { 
+      theme: { 
+        extend: { 
+          screens: { 'sm': '425px', 
+         } } }
+        }
+  </script> */
+}
